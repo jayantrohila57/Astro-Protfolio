5485681ec7ea4b8280e97ff9aae2c5b9acaf13a0
Jayant Rohila <jrohila55@gmail.com>
2023-03-01 15:28:40 +0530

    update template

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const headerData = {
+  links: [],
+  actions: [{ type: 'button', text: 'WhatsApp', href: 'whatsapp://send?abid=8445987241&text=Hello' }],
+}
+
+export const footerData = {
+  links: [],
+  secondaryLinks: [],
+  socialLinks: [],
+  footNote: '',
+}
